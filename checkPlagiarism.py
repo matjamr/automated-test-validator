668d491127fe4ec1b018436243796ce41f070045
Grzegorz Pustulka <grzegorzpustulka@onet.pl>
2023-03-15 20:42:10 +0100

    add a new test to plagiarism

--- checkPlagiarism.py
+++ checkPlagiarism.py
@@ -2,6 +2,20 @@ import difflib
 import json
 import glob
 import re
+import pandas as pd
+from copydetect import CopyDetector
+
+# TODO[GRZEGORZ]: WIECEJ SKIPOW ZADAN W LAB1, GDY DOSTANE PLIKI DO INNYCH LAB
+
+# TODO[Grzegorz]: zrobic testy na wiekszych ilosciach danych i kolejnych labolatoriow
+# TODO[Grzegorz]: sprawdzic jak ratio wychodzi na plagiacie, ktory byl lekko refactorowany (zmienne, printy)
+# TODO[Grzegorz]: rodzielic te klase na CheckPlagiarsim, loadFile, GenerateReport
+# TODO[Grzegorz]: przeniesc klase ChcekPlagiarism do app
+
+
+def import_tasks_to_skip(lab_num):
+    df = pd.read_excel("tasksToBeMissed/data.xlsx")
+    return df[int(lab_num[-7])].tolist()
 
 
 class CheckPlagiarism:
@@ -40,14 +54,19 @@ class CheckPlagiarism:
             number_task = 1
 
     def task_comparison(self):
-
         students_names = list(self.students_tasks.keys())
+        tasks_to_skip = import_tasks_to_skip(students_names[0])
         task_names = []
+
         for keys, values in self.students_tasks.items():
             for key in values.keys():
-                task_names.append(key)
+                if key in tasks_to_skip:
+                    continue
+                else:
+                    task_names.append(key)
             break
 
+        # TODO[Grzegorz] : zmniejszyc Notacja dużego O, priorytet - niski  (bo mi sie nie chce myslec)
         for task in task_names:
             for i in range(len(students_names)):
                 for j in range(i + 1, len(students_names)):
@@ -55,8 +74,25 @@ class CheckPlagiarism:
                         differ = difflib.SequenceMatcher(None, self.students_tasks[students_names[i]][task],
                                                          self.students_tasks[students_names[j]][task])
                         similarity_ratio = differ.ratio()
-                        if similarity_ratio > 0.7:
+                        if similarity_ratio > 0.85:
                             print(f"{students_names[i]} i {students_names[j]} {task} Podobienstwo: {similarity_ratio}")
 
+                            file1 = open(f"result/{students_names[i]}_{task}.py", "a", encoding='utf-8')
+                            file2 = open(f"result/{students_names[j]}_{task}.py", "a", encoding='utf-8')
+                            file1.write(f"{self.students_tasks[students_names[i]][task]}")
+                            file2.write(f"{self.students_tasks[students_names[j]][task]}")
+                            file1.close()
+                            file2.close()
                     except IndexError:
                         continue
+
+        detector = CopyDetector(test_dirs=["result/"], extensions=["py"], display_t=0.5)
+        # detector.add_file("copydetect/utils.py")
+        detector.run()
+        detector.generate_html_report()
+
+
+
+    def generate_report(self, student_a, student_b, task_number, similarity):
+        # TODO[Grzegorz]: dodac generowanie raportow (w nowej klasie)
+        pass
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from app.AppEngine import AppEngine
 from checkPlagiarism import CheckPlagiarism
 
 if __name__ == '__main__':
-    appEngine = AppEngine()
-    appEngine.start()
+    # appEngine = AppEngine()
+    # appEngine.start()
 
+    # TODO[Grzegorz] : zmienic na metode start
     checkPlagiarism = CheckPlagiarism()
     checkPlagiarism.load_file()
     checkPlagiarism.download_tasks()
