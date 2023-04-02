bd7f9e6972c29bd2f99c430975d9c99bb799ead6
Grzegorz Pustulka <grzegorzpustulka@onet.pl>
2023-04-02 21:37:07 +0200

    Refactor

--- app/executors/PlagiarismExecutor.py
+++ app/executors/PlagiarismExecutor.py
@@ -1,6 +1,6 @@
 from app.context.ValidatorContext import ValidatorContext
 from app.executors.BaseExecutor import BaseExecutor
-
+from app.actions.helper_actions import load_file, download_tasks
 import difflib
 import pandas as pd
 import copydetect
@@ -8,6 +8,9 @@ import os
 
 
 class PlagiarismExecutor(BaseExecutor):
+    def __init__(self):
+        self.json_data = load_file()
+        self.students_tasks = download_tasks(self.json_data)
 
     def should_execute(self) -> bool:
         return True
@@ -20,7 +23,7 @@ class PlagiarismExecutor(BaseExecutor):
         for task in task_names:
             self.compare_students(task, students_names)
 
-    def get_task_names(self, tasks_to_skip):
+    def get_task_names(self, tasks_to_skip: list[str]) -> list[str]:
         task_names = []
         for values in self.students_tasks.values():
             for key in values.keys():
@@ -29,40 +32,40 @@ class PlagiarismExecutor(BaseExecutor):
             break
         return task_names
 
-    def compare_students(self, task, students_names):
+    def compare_students(self, task: str, students_names) -> None:
         for i in range(len(students_names)):
             for j in range(i + 1, len(students_names)):
                 try:
                     similarity_ratio = self.get_similarity_ratio(students_names, i, j, task)
-                    if similarity_ratio > 0.85:
+                    if similarity_ratio > 0.95:
                         self.write_files(students_names, i, j, task)
                         similarity_score = get_similarity_score(students_names, i, j, task)
-                        if similarity_score > 0.85:
+                        if similarity_score >= 0.99:
                             print_result(students_names, i, j, task, similarity_ratio, similarity_score)
                             clean_up_files()
 
                 except IndexError:
                     continue
 
-    def get_similarity_ratio(self, students_names, i, j, task):
+    def get_similarity_ratio(self, students_names: list[int], i: int, j: int, task: str) -> float:
         differ = difflib.SequenceMatcher(None, self.students_tasks[students_names[i]][task],
                                          self.students_tasks[students_names[j]][task])
         return differ.ratio()
 
-    def write_files(self, students_names, i, j, task):
+    def write_files(self, students_names: list[int], i: int, j: int, task: str) -> None:
         with open(f"result/{students_names[i]}_{task}.py", "a", encoding='utf-8') as file1:
             file1.write(f"{self.students_tasks[students_names[i]][task]}")
         with open(f"result/{students_names[j]}_{task}.py", "a", encoding='utf-8') as file2:
             file2.write(f"{self.students_tasks[students_names[j]][task]}")
 
 
-def import_tasks_to_skip(lab_num: str):
+def import_tasks_to_skip(lab_num: str) -> list[str]:
     df = pd.read_excel("tasksToBeMissed/data.xlsx")
     # lab_num[-7] is laboratory number "name_laboratoryNumber.ipynb"
     return df[int(lab_num[-7])].tolist()
 
 
-def get_similarity_score(students_names: list[str], i: int, j: int, task: str):
+def get_similarity_score(students_names: list[str], i: int, j: int, task: str) -> float:
     file_path1 = f"result/{students_names[i]}_{task}.py"
     file_path2 = f"result/{students_names[j]}_{task}.py"
     fp1 = copydetect.CodeFingerprint(file_path1, 25, 1)
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from app.AppEngine import AppEngine
-from checkPlagiarism import CheckPlagiarism
 
 if __name__ == '__main__':
     appEngine = AppEngine()
     appEngine.start()
-    # checkPlagiarism = CheckPlagiarism()
-    # checkPlagiarism.start()
+
 
